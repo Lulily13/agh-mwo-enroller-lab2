d3fb6ed6625eaaafc47d8842a9944b587fe940b7
Lulily13 <bloodyakuma13@yahoo.com>
2025-05-08 17:37:16 +0200

    chore: add JWT authentication and configure Spring Security with password encoder and token-based authentication

--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -1,15 +1,36 @@
 package com.company.enroller.security;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ParticipantProvider participantProvider;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Value("${security.secret}")
+    private String secret;
+
+    @Value("${security.issuer}")
+    private String issuer;
+
+    @Value("${security.token_expiration_in_seconds}")
+    private int tokenExpiration;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -17,10 +38,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        http.addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), secret, issuer, tokenExpiration), UsernamePasswordAuthenticationFilter.class);
     }
 }
